699d31eb37a2eb4537a43bfb2bf909cc13e790f5
Moohen <mateusz.czekala.masazysta@gmail.com>
2017-12-10 19:22:10 +0100

    Initial commit

---
+++ src/com/mateusz/Main.java
@@ -0,0 +1,12 @@
+package com.mateusz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String test;
+
+        test = "Dlaczego zapisuje mi z innego konta?";
+        System.out.println(test);
+
+    }
+}
